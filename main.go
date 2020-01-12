9a0d08ef79c71f5196c1cbff1d890069f6870185
Chathan Driehuys <chathan@driehuys.com>
2020-01-12 11:42:49 -0500

    Write basic PPM to disk.

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+)
 
 type projectile struct {
 	Position Tuple
@@ -23,6 +27,32 @@ func main() {
 		tickCount += 1
 		summarize(tickCount, proj)
 	}
+
+	canvas := MakeCanvas(600, 900)
+
+	for x := 275; x <= 325; x++ {
+		for y := 425; y <= 475; y++ {
+			canvas.SetPixel(x, y, MakeColor(1, 0, 0))
+		}
+	}
+
+	file, err := os.Create("output.ppm")
+	if err != nil {
+		log.Fatalf("Failed to open 'output.ppm': %v", err)
+	}
+
+	defer func() {
+		if err := file.Close(); err != nil {
+			log.Fatalf("Failed to close 'output.ppm': %v", err)
+		}
+	}()
+
+	log.Println("Writing PPM image to file.")
+	ppm := Pixmap{canvas, file}
+	if err := ppm.Write(); err != nil {
+		log.Fatalf("Failed to write PPM file: %v", err)
+	}
+	log.Println("Finished writing PPM image.")
 }
 
 func tick(env environment, proj projectile) projectile {
