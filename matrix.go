3f7ba39ca449111a2c0c11095652bb5cba218bbc
Chathan Driehuys <chathan@driehuys.com>
2020-01-19 14:19:05 -0500

    Calculating determinants of matrices.

--- matrix.go
+++ matrix.go
@@ -44,6 +44,40 @@ func MakeMatrix4(v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v1
 	}
 }
 
+var (
+	IdentityMatrix4 = MakeMatrix4(
+		1, 0, 0, 0,
+		0, 1, 0, 0,
+		0, 0, 1, 0,
+		0, 0, 0, 1,
+	)
+)
+
+// Find a cofactor in the matrix.
+func (m Matrix) Cofactor(row, col int) float64 {
+	minor := m.Minor(row, col)
+
+	if (row+col)%2 == 1 {
+		return -minor
+	}
+
+	return minor
+}
+
+// Find the determinant of the matrix.
+func (m Matrix) Determinant() float64 {
+	if m.Size == 2 {
+		return m.Get(0, 0)*m.Get(1, 1) - m.Get(0, 1)*m.Get(1, 0)
+	}
+
+	determinant := 0.0
+	for col := 0; col < m.Size; col++ {
+		determinant += m.Get(0, col) * m.Cofactor(0, col)
+	}
+
+	return determinant
+}
+
 // Get the value at a specific row and column in the matrix.
 func (m Matrix) Get(row, column int) float64 {
 	return m.store[row*m.Size+column]
@@ -65,4 +99,103 @@ func (m Matrix) Equals(other Matrix) bool {
 	}
 
 	return true
+}
+
+// Find the minor of a matrix which is defined as the determinant of its
+// submatrix. The submatrix is determined by the given row and column, which are
+// removed from the matrix to produce the submatrix.
+func (m Matrix) Minor(row, col int) float64 {
+	return m.Submatrix(row, col).Determinant()
+}
+
+// Multiply this matrix by another.
+func (m Matrix) Multiply(b Matrix) Matrix {
+	c := Matrix{Size: m.Size, store: make([]float64, m.Size*m.Size)}
+
+	for row := 0; row < m.Size; row++ {
+		for col := 0; col < m.Size; col++ {
+			cellValue := 0.0
+			for i := 0; i < m.Size; i++ {
+				aVal := m.Get(row, i)
+				bVal := b.Get(i, col)
+				cellValue += aVal * bVal
+			}
+			c.Set(row, col, cellValue)
+		}
+	}
+
+	return c
+}
+
+func (m *Matrix) Set(row, column int, value float64) {
+	m.store[row*m.Size+column] = value
+}
+
+// Create a matrix that is a copy of the current matrix but with a specific row
+// and column removed.
+func (m Matrix) Submatrix(removedRow, removedCol int) Matrix {
+	targetRow := 0
+	targetCol := 0
+	c := Matrix{
+		Size:  m.Size - 1,
+		store: make([]float64, (m.Size-1)*(m.Size-1)),
+	}
+
+	for row := 0; row < m.Size; row++ {
+		if row == removedRow {
+			continue
+		}
+
+		targetCol = 0
+		for col := 0; col < m.Size; col++ {
+			if col == removedCol {
+				continue
+			}
+
+			c.Set(targetRow, targetCol, m.Get(row, col))
+
+			targetCol++
+		}
+		targetRow++
+	}
+
+	return c
+}
+
+// Get the transpose of the target matrix.
+func (m Matrix) Transposed() Matrix {
+	c := Matrix{
+		Size:  m.Size,
+		store: make([]float64, m.Size*m.Size),
+	}
+
+	for row := 0; row < m.Size; row++ {
+		for col := 0; col < m.Size; col++ {
+			c.Set(col, row, m.Get(row, col))
+		}
+	}
+
+	return c
+}
+
+// Multiply the target matrix by a tuple.
+func (m Matrix) TupleMultiply(b Tuple) Tuple {
+	return Tuple{
+		X: m.Get(0, 0)*b.X +
+			m.Get(0, 1)*b.Y +
+			m.Get(0, 2)*b.Z +
+			m.Get(0, 3)*b.W,
+		Y: m.Get(1, 0)*b.X +
+			m.Get(1, 1)*b.Y +
+			m.Get(1, 2)*b.Z +
+			m.Get(1, 3)*b.W,
+		Z: m.Get(2, 0)*b.X +
+			m.Get(2, 1)*b.Y +
+			m.Get(2, 2)*b.Z +
+			m.Get(2, 3)*b.W,
+		W: m.Get(3, 0)*b.X +
+			m.Get(3, 1)*b.Y +
+			m.Get(3, 2)*b.Z +
+			m.Get(3, 3)*b.W,
+	}
 }
--- matrix_test.go
+++ matrix_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestMakeMatrix2(t *testing.T) {
 	matrix := MakeMatrix2(
@@ -41,6 +44,118 @@ func TestMakeMatrix4(t *testing.T) {
 	assertMatrixValue(t, matrix, 2, 2, 11)
 	assertMatrixValue(t, matrix, 3, 0, 13.5)
 	assertMatrixValue(t, matrix, 3, 2, 15.5)
+}
+
+func TestMatrix_Cofactor(t *testing.T) {
+	type matrixTestCase struct {
+		row          int
+		col          int
+		wantMinor    float64
+		wantCofactor float64
+	}
+	testCases := []struct {
+		name   string
+		matrix Matrix
+		tests  []matrixTestCase
+	}{
+		{
+			"3x3",
+			MakeMatrix3(
+				3, 5, 0,
+				2, -1, -7,
+				6, -1, 5,
+			),
+			[]matrixTestCase{
+				{0, 0, -12, -12},
+				{1, 0, 25, -25},
+			},
+		},
+	}
+	for _, tt := range testCases {
+		t.Run(tt.name, func(t *testing.T) {
+			for _, testCase := range tt.tests {
+				t.Run(fmt.Sprintf("Row %d Column %d", testCase.row, testCase.col), func(t *testing.T) {
+					minor := tt.matrix.Minor(testCase.row, testCase.col)
+					if !Float64Equal(minor, testCase.wantMinor) {
+						t.Errorf("Expected minor(A, %d, %d) = %v, got %v\nA = %v", testCase.row, testCase.col, testCase.wantMinor, minor, tt.matrix)
+					}
+
+					cofactor := tt.matrix.Cofactor(testCase.row, testCase.col)
+					if !Float64Equal(cofactor, testCase.wantCofactor) {
+						t.Errorf("Expected cofactor(A, %d, %d) = %v, got %v\nA = %v", testCase.row, testCase.col, testCase.wantCofactor, cofactor, tt.matrix)
+					}
+				})
+			}
+		})
+	}
+}
+
+func TestMatrix_Determinant(t *testing.T) {
+	type cofactorTest struct {
+		row  int
+		col  int
+		want float64
+	}
+	testCases := []struct {
+		name          string
+		a             Matrix
+		cofactorTests []cofactorTest
+		want          float64
+	}{
+		{
+			name: "2x2",
+			a: MakeMatrix2(
+				1, 5,
+				-3, 2,
+			),
+			want: 17,
+		},
+		{
+			name: "3x3",
+			a: MakeMatrix3(
+				1, 2, 6,
+				-5, 8, -4,
+				2, 6, 4,
+			),
+			cofactorTests: []cofactorTest{
+				{0, 0, 56},
+				{0, 1, 12},
+				{0, 2, -46},
+			},
+			want: -196,
+		},
+		{
+			name: "4x4",
+			a: MakeMatrix4(
+				-2, -8, 3, 5,
+				-3, 1, 7, 3,
+				1, 2, -9, 6,
+				-6, 7, 7, -9,
+			),
+			cofactorTests: []cofactorTest{
+				{0, 0, 690},
+				{0, 1, 447},
+				{0, 2, 210},
+				{0, 3, 51},
+			},
+			want: -4071,
+		},
+	}
+	for _, tt := range testCases {
+		t.Run(tt.name, func(t *testing.T) {
+			for _, ct := range tt.cofactorTests {
+				t.Run(fmt.Sprintf("cofactor row %d column %d", ct.row, ct.col), func(t *testing.T) {
+					if got := tt.a.Cofactor(ct.row, ct.col); !Float64Equal(got, ct.want) {
+						t.Errorf("Expected cofactor(A, %d, %d) = %v, got %v\nA = %v", ct.row, ct.col, ct.want, got, tt.a)
+					}
+				})
+			}
+
+			if got := tt.a.Determinant(); !Float64Equal(got, tt.want) {
+				t.Errorf("Expected determinant(A) = %v, got %v\nA = %v", tt.want, got, tt.a)
+			}
+		})
+	}
 }
 
 func TestMatrix_Equals(t *testing.T) {
@@ -104,6 +219,207 @@ func TestMatrix_Equals(t *testing.T) {
 					t.Errorf("Expected A != B\nA = %v\nB = %v", tt.m1, tt.m2)
 
 				}
+			}
+		})
+	}
+}
+
+func TestMatrix_Minor(t *testing.T) {
+	testCases := []struct {
+		name string
+		a    Matrix
+		row  int
+		col  int
+		want float64
+	}{
+		{
+			"3x3",
+			MakeMatrix3(
+				3, 5, 0,
+				2, -1, -7,
+				6, -1, 5,
+			),
+			1,
+			0,
+			25,
+		},
+	}
+	for _, tt := range testCases {
+		b := tt.a.Submatrix(tt.row, tt.col)
+		determinant := b.Determinant()
+
+		if !Float64Equal(determinant, tt.want) {
+			t.Errorf("Expected determinant(submatrix(A, %d, %d)) = %v, got %v\nA = %v", tt.row, tt.col, tt.want, determinant, tt.a)
+		}
+
+		minor := tt.a.Minor(tt.row, tt.col)
+		if !Float64Equal(minor, tt.want) {
+			t.Errorf("Expected minor(A, %d, %d) = %v, got %v\nA = %v", tt.row, tt.col, tt.want, minor, tt.a)
+		}
+	}
+}
+
+func TestMatrix_Multiply(t *testing.T) {
+	testCases := []struct {
+		name string
+		a    Matrix
+		b    Matrix
+		want Matrix
+	}{
+		{
+			"4x4",
+			MakeMatrix4(
+				1, 2, 3, 4,
+				5, 6, 7, 8,
+				9, 8, 7, 6,
+				5, 4, 3, 2,
+			),
+			MakeMatrix4(
+				-2, 1, 2, 3,
+				3, 2, 1, -1,
+				4, 3, 6, 5,
+				1, 2, 7, 8,
+			),
+			MakeMatrix4(
+				20, 22, 50, 48,
+				44, 54, 114, 108,
+				40, 58, 110, 102,
+				16, 26, 46, 42,
+			),
+		},
+		{
+			"4x4 identity",
+			MakeMatrix4(
+				0, 1, 2, 4,
+				1, 2, 4, 8,
+				2, 4, 8, 16,
+				4, 8, 16, 32,
+			),
+			IdentityMatrix4,
+			MakeMatrix4(
+				0, 1, 2, 4,
+				1, 2, 4, 8,
+				2, 4, 8, 16,
+				4, 8, 16, 32,
+			),
+		},
+	}
+	for _, tt := range testCases {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.a.Multiply(tt.b); !got.Equals(tt.want) {
+				t.Errorf("Expected A x B = C, got D:\nA = %v\nB = %v\nC = %v\nD = %v", tt.a, tt.b, tt.want, got)
+			}
+		})
+	}
+}
+
+func TestMatrix_Submatrix(t *testing.T) {
+	testCases := []struct {
+		name string
+		a    Matrix
+		row  int
+		col  int
+		want Matrix
+	}{
+		{
+			"3x3 to 2x2",
+			MakeMatrix3(
+				1, 5, 0,
+				-3, 2, 7,
+				0, 6, -3,
+			),
+			0,
+			2,
+			MakeMatrix2(
+				-3, 2,
+				0, 6,
+			),
+		},
+		{
+			"4x4 to 3x3",
+			MakeMatrix4(
+				-6, 1, 1, 6,
+				-8, 5, 8, 6,
+				-1, 0, 8, 2,
+				-7, 1, -1, 1,
+			),
+			2,
+			1,
+			MakeMatrix3(
+				-6, 1, 6,
+				-8, 8, 6,
+				-7, -1, 1,
+			),
+		},
+	}
+	for _, tt := range testCases {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.a.Submatrix(tt.row, tt.col); !got.Equals(tt.want) {
+				t.Errorf("Expected submatrix(A, %d, %d) = B, got C\nA = %v\nB = %v\nC = %v", tt.row, tt.col, tt.a, tt.want, got)
+			}
+		})
+	}
+}
+
+func TestMatrix_Transposed(t *testing.T) {
+	testCases := []struct {
+		name string
+		a    Matrix
+		want Matrix
+	}{
+		{
+			"4x4 identity",
+			IdentityMatrix4,
+			IdentityMatrix4,
+		},
+		{
+			"4x4",
+			MakeMatrix4(
+				0, 9, 3, 0,
+				9, 8, 0, 8,
+				1, 8, 5, 3,
+				0, 0, 5, 8,
+			),
+			MakeMatrix4(
+				0, 9, 1, 0,
+				9, 8, 8, 0,
+				3, 0, 5, 5,
+				0, 8, 3, 8,
+			),
+		},
+	}
+	for _, tt := range testCases {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.a.Transposed(); !got.Equals(tt.want) {
+				t.Errorf("Expected transpose(A) = B, got C\nA = %v\nB = %v\nC = %v", tt.a, tt.want, got)
+			}
+		})
+	}
+}
+
+func TestMatrix_TupleMultiply(t *testing.T) {
+	testCases := []struct {
+		name string
+		a    Matrix
+		b    Tuple
+		want Tuple
+	}{
+		{
+			"4x4",
+			MakeMatrix4(
+				1, 2, 3, 4,
+				2, 4, 4, 2,
+				8, 6, 4, 1,
+				0, 0, 0, 1,
+			),
+			Tuple{1, 2, 3, 1},
+			Tuple{18, 24, 33, 1},
+		},
+	}
+	for _, tt := range testCases {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.a.TupleMultiply(tt.b); !got.Equals(tt.want) {
+				t.Errorf("Expected A x b = c, got d:\nA = %v\nb = %v\nc = %v\nd = %v", tt.a, tt.b, tt.want, got)
 			}
 		})
 	}
