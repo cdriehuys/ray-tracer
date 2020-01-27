86681c490b6c33d548b6980bddb76f9bf420f7f2
Chathan Driehuys <chathan@driehuys.com>
2020-01-26 19:21:27 -0500

    Add material lighting calculation.
    We can now calculate the color at a specific position based on the
    material, light source, eye position, and normal vector of the surface.

--- light.go
+++ light.go
@@ -1,5 +1,7 @@
 package main
 
+import "math"
+
 type PointLight struct {
 	Position  Tuple
 	Intensity Color
@@ -10,4 +12,42 @@ func MakePointLight(position Tuple, intensity Color) PointLight {
 		Position:  position,
 		Intensity: intensity,
 	}
+}
+
+// Get the color of a position given a material, light source, observer, and the
+// normal of the illuminated surface.
+func Lighting(material Material, light PointLight, position Tuple, eyeVector Tuple, normal Tuple) Color {
+	// Initial color is a combination of the material's color and the light's
+	// color.
+	effectiveColor := material.Color.Blend(light.Intensity)
+
+	lightVector := light.Position.Subtract(position).Normalized()
+
+	// The ambient color is the color contribution from "background" light or
+	// the color shown with no light sources.
+	ambient := effectiveColor.Multiply(material.Ambient)
+
+	diffuse := MakeColor(0, 0, 0)
+	specular := MakeColor(0, 0, 0)
+	// The dot product of the vector to the light source and the normal vector
+	// represents the cosine of the angle between the two. We only need to
+	// compute the diffuse and specular colors if the cosine is positive,
+	// because a negative result indicates the light source is on the other side
+	// of the surface.
+	lightDotNormal := lightVector.Dot(normal)
+	if lightDotNormal >= 0 {
+		diffuse = effectiveColor.Multiply(material.Diffuse).Multiply(lightDotNormal)
+
+		// The following represents the cosine of the angle between the
+		// reflection vector and they eye vector. A negative number means the
+		// light reflects away from the eye.
+		reflectionVector := Reflect(lightVector.Multiply(-1), normal)
+		reflectionDotEye := reflectionVector.Dot(eyeVector)
+		if reflectionDotEye > 0 {
+			factor := math.Pow(reflectionDotEye, material.Shininess)
+			specular = light.Intensity.Multiply(material.Specular).Multiply(factor)
+		}
+	}
+
+	return ambient.Add(diffuse).Add(specular)
 }
--- light_test.go
+++ light_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"math"
+	"testing"
+)
 
 func TestMakePointLight(t *testing.T) {
 	intensity := MakeColor(1, 1, 1)
@@ -14,5 +17,60 @@ func TestMakePointLight(t *testing.T) {
 
 	if got := light.Intensity; !got.Equals(intensity) {
 		t.Errorf("Expected light intensity to be %v, got %v", intensity, got)
+	}
+}
+
+func TestLighting(t *testing.T) {
+	testCases := []struct {
+		name      string
+		eyeVector Tuple
+		normal    Tuple
+		light     PointLight
+		want      Color
+	}{
+		{
+			"eye between light and surface",
+			MakeVector(0, 0, -1),
+			MakeVector(0, 0, -1),
+			MakePointLight(MakePoint(0, 0, -10), MakeColor(1, 1, 1)),
+			MakeColor(1.9, 1.9, 1.9),
+		},
+		{
+			"eye between light and surface - eye offset 45 degrees",
+			MakeVector(0, math.Sqrt2/2, -math.Sqrt2/2),
+			MakeVector(0, 0, -1),
+			MakePointLight(MakePoint(0, 0, -10), MakeColor(1, 1, 1)),
+			MakeColor(1, 1, 1),
+		},
+		{
+			"eye opposite surface - light offset 45 degrees",
+			MakeVector(0, 0, -1),
+			MakeVector(0, 0, -1),
+			MakePointLight(MakePoint(0, 10, -10), MakeColor(1, 1, 1)),
+			MakeColor(0.7364, 0.7364, 0.7364),
+		},
+		{
+			"eye in path of reflection vector",
+			MakeVector(0, -math.Sqrt2/2, -math.Sqrt2/2),
+			MakeVector(0, 0, -1),
+			MakePointLight(MakePoint(0, 10, -10), MakeColor(1, 1, 1)),
+			MakeColor(1.6364, 1.6364, 1.6364),
+		},
+		{
+			"light behind surface",
+			MakeVector(0, 0, -1),
+			MakeVector(0, 0, -1),
+			MakePointLight(MakePoint(0, 0, 10), MakeColor(1, 1, 1)),
+			MakeColor(0.1, 0.1, 0.1),
+		},
+	}
+	for _, tt := range testCases {
+		m := MakeMaterial()
+		position := MakePoint(0, 0, 0)
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Lighting(m, tt.light, position, tt.eyeVector, tt.normal); !got.Equals(tt.want) {
+				t.Errorf("Expected lighting to produce color %v, got %v", tt.want, got)
+			}
+		})
 	}
 }
